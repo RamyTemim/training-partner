3720ac8879b1306d0bc2b35b343cd10e9f39384d
Martin Trullard <martin@macbook-pro-de-martin.home>
2023-03-12 15:58:49 +0100

    correction nomanclature

--- training-partner/src/Components/App/App.tsx
+++ training-partner/src/Components/App/App.tsx
@@ -8,7 +8,7 @@ import './App.css';
 import Formulaire from '../../Containers/Formulaire/Formulaire';
 import { Bar } from 'react-chartjs-2';
 import BarChart from '../Bar/BarChart';
-import Tab_bord from '../Elements_fixe/tableau_de_bord /tab_bord';
+import Tab_bord from '../Elements_fixe/tableau_de_bord/tab_bord';
 import Header from '../Elements_fixe/barre_top/head';
 function App() {    
  return(
