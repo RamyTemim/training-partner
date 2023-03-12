bfd89b9b901411c4fad0b34e212c5c11e8c039c3
Martin Trullard <martin@macbook-pro-de-martin.home>
2023-03-12 15:56:04 +0100

    correction bug

--- training-partner/src/Components/App/App.tsx
+++ training-partner/src/Components/App/App.tsx
@@ -8,12 +8,14 @@ import './App.css';
 import Formulaire from '../../Containers/Formulaire/Formulaire';
 import { Bar } from 'react-chartjs-2';
 import BarChart from '../Bar/BarChart';
-import Tab_bord from '../Elements_fixe/tableau_de_bord /tab_bord';
+import Tab_bord from '../Elements_fixe/tableau_de_bord/tab_bord';
 import Header from '../Elements_fixe/barre_top/head';
+import HomeChart from'../Pages/page_chart/page_chart';
 function App() {    
  return(
   <div className='App'>
     <Header/>
+    <HomeChart/>
     <Tab_bord/>
   </div>
  );
