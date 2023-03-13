8cbb546980a8f66456a81d64b44cdb4bb7bef4b3
gableul <gabriel.leulmi@gmail.com>
2023-03-13 13:37:35 +0100

    bug nom tabbord

---
+++ training-partner/src/Components/Pages/PageChart/PageChart.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+function HomeChart(){
+    return (
+        <div>
+
+            <h1>
+            bonjour
+            </h1>
+
+        </div>
+    );
+
+}
+
+export default HomeChart;
