262eb456352f11571b827338712a8ad67b8783df
gableul <gabriel.leulmi@gmail.com>
2023-03-13 13:29:57 +0100

    rectification nomenclature CamelCase

--- training-partner/src/Components/App/App.tsx
+++ training-partner/src/Components/App/App.tsx
@@ -8,8 +8,8 @@ import './App.css';
 import Formulaire from '../../Containers/Formulaire/Formulaire';
 import { Bar } from 'react-chartjs-2';
 import BarChart from '../Bar/BarChart';
-import Tab_bord from '../Elements_fixe/tableau_de_bord/tab_bord';
-import Header from '../Elements_fixe/barre_top/head';
+import Tab_bord from '../ElementsFixe/TableauDeBord/TabBord';
+import Header from '../ElementsFixe/barreTop/head';
 function App() {    
  return(
   <div className='App'>
