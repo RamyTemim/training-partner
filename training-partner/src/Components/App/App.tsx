095fdd64c36de651a3ca5eab9db2dcd4cb34f3cd
John010203 <jonathanlong52@gmail.com>
2023-04-05 14:52:57 +0200

    correctifs positions

--- training-partner/src/Components/App/App.tsx
+++ training-partner/src/Components/App/App.tsx
@@ -20,42 +20,56 @@ function App() {
   const [chartCreate,setChartCreate] = useState(false);
   const [Battle, setBattle] = useState(false);
   
-  
+  useEffect(() => {
+    const message = {
+      name: "John Doe",
+      age: 30,
+      email: "jongenshin@example.com"
+    };
+    fetch('http://localhost:3000/bite', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(message)
+    })
+      .then(response => response.text())
+      .then(data => console.log(data))
+  }, []);
   
   const clickcv = (page : number)=>{
-    if (page == 0){
+    if (page ==0){
       setAccueil(true);setActivite(false);setConseil(false);setChartCreate(false);setChartVisu(false);setBattle(false);
     }
     if (page == 1){
       setActivite(true);setConseil(false);setChartCreate(false);setChartVisu(false);setBattle(false);setAccueil(false);
     }
-    if (page == 2){
+    if (page ==2){
       setConseil(true);setChartCreate(false);setChartVisu(false);setBattle(false);setAccueil(false);setActivite(false);
     }
-    if (page == 3){
+    if (page ==3){
       setChartVisu(true);setChartCreate(false);setBattle(false);setAccueil(false);setActivite(false);setConseil(false);
    }
     if (page == 4){
       setChartCreate(true);setChartVisu(false);setBattle(false);setAccueil(false);setActivite(false);setConseil(false);
      }
-    if (page == 5){
+    if (page ==5){
       setBattle(true);setAccueil(false);setActivite(false);setConseil(false);setChartCreate(false);setChartVisu(false);
     }
-    }
-return(
-  <div className='App'>
+  }
 
-    <TabBord onPageChange ={clickcv}/>
-    <Header/>
-    {Accueil && null} 
-    {Activite && <PageActivite/>}
-    {Conseil && null}
-    {chartCreate && <ChartCreate/>}
-    {chartVisu && <ChartVisu/>}
-    {Battle && <PageBattle/>}
-
-  </div>
- );
+  return(
+    <div className='App'>
+      <TabBord onPageChange ={clickcv}/>
+      <Header/>
+      {Accueil && null} 
+      {Activite && <PageActivite/>}
+      {Conseil && null}
+      {chartCreate && <ChartCreate/>}
+      {chartVisu && <ChartVisu/>}
+      {Battle && <PageBattle/>}
+    </div>
+  );
 }
 
 export default App;
