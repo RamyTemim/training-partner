1b7ef63a80878c7b46273b629218d92ca84312fc
gableul <gabriel.leulmi@gmail.com>
2023-04-05 18:28:00 +0200

    changement graphique

--- training-partner/src/Components/Pages/Activite/FormSeance/FormMuscu.tsx
+++ training-partner/src/Components/Pages/Activite/FormSeance/FormMuscu.tsx
@@ -7,7 +7,7 @@ interface Muscu{
     poids: number;
     restTime: string;
 }
-
+var triangle:any;
 
 const FormMuscu: React.FC= ()=>{
     const [valeur,setValeur]=useState<Muscu>({
@@ -28,7 +28,7 @@ const FormMuscu: React.FC= ()=>{
     
     const handleSubmit= (event: React.FormEvent<HTMLFormElement>)=>{
         event.preventDefault();
-        if (valeur.poids==0 || valeur.nbrRep==0 || valeur.nom=="" ||valeur.nbrSerie==0 ){
+        if (valeur.nbrRep==0 || valeur.nom=="" ||valeur.nbrSerie==0 ){
             alert("Veuillez remplir tous les champs obligatoires");
         }else{
         setExercices([...exercices, valeur]);
@@ -76,15 +76,19 @@ const FormMuscu: React.FC= ()=>{
             </form>
             <div className="ListEx">
             <h4>Liste des exercices </h4>
-                <ul>
+                <ul className="exercice">
                     {exercices.map((exercice, index) => (
                         <li key={index}>
-                            <button id="buttonExercice" onClick={() => showDetails(index)}>{exercice.nom}</button>
+                            <button id="buttonExercice" onClick={() => showDetails(index)}>
+                                {exercice.nom}<svg className="fleche" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </button>
                             <svg xmlns="http://www.w3.org/2000/svg" className="trashButton" onClick={() => handleDelete(index)} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                             </svg>
                             {details === index && (
-                                <ul>
+                                <ul  className="exercice">
                                     <li>Nombre de séries : {exercice.nbrSerie}</li>
                                     <li>Nombre de répétitions : {exercice.nbrRep}</li>
                                     <li>Poids (en kg) : {exercice.poids}</li>
