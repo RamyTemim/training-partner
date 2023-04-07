49da27dd4fc177c67ed53b04caa2b6e4433e7157
John010203 <jonathanlong52@gmail.com>
2023-04-07 11:01:48 +0200

    supression calendrier

--- training-partner/src/Components/Pages/Activite/Activite.tsx
+++ training-partner/src/Components/Pages/Activite/Activite.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Header from "../../ElementsFixe/BarreTop/head";
 import TabBord from "../../ElementsFixe/TableauDeBord/TabBord";
-import Calendrier from "./FormSeance/Calendrier/Calendrier";
 import FormSeance from "./FormSeance/FormSeance";
 import SelectSport from "./FormSeance/SelectSport/SelectSport";
 import Seance from "./Seance/Seance";
@@ -10,9 +9,7 @@ import SportsList from "./Sport/Sport";
 function PageActivite(){
     return (
         <div>
-
            <FormSeance/>
-  
         </div>
     )
 }
