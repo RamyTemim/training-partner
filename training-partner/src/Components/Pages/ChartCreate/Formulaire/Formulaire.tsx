8372f6f4823b1632b761d7b3567f51dadcbef103
gableul <gabriel.leulmi@gmail.com>
2023-04-25 22:21:02 +0200

    update_reset_chartdonne_onSave

--- training-partner/src/Components/Pages/ChartCreate/Formulaire/Formulaire.tsx
+++ training-partner/src/Components/Pages/ChartCreate/Formulaire/Formulaire.tsx
@@ -169,6 +169,28 @@ const Formulaire: React.FC = () => {
     catch(error){
       console.error(error)
     }
+    setchartDonnee({
+      labels: [] as string[],
+      datasets: [
+        {
+          label:formSave.titre,
+  
+          data: [] as number[],
+          //couleur prédefinies
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(153, 102, 255, 0.5)',
+            'rgba(255, 159, 64, 0.5)',
+          ],
+  
+          borderColor: 'DDDDDD',
+          borderWidth: 2,
+        },
+      ],
+    });
     setformSave({titre:"",sport:"musculation"});
   }
 
@@ -247,9 +269,11 @@ const Formulaire: React.FC = () => {
                 <tr key={index}>
                   <td>{value}</td>
                   <td>{chartDonnee.datasets[0].data[index]}</td>
-                  <td><svg xmlns="http://www.w3.org/2000/svg" className="trashButton" onClick={() => handleDelete(value)} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                      </svg></td>  
+                  <td>  
+                    <svg xmlns="http://www.w3.org/2000/svg" className="trashButton" onClick={() => handleDelete(value)} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+                  </td>  
                 </tr>
               ))}
             </tbody>
